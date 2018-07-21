f83756297eeedf36732620e29bc6064a804b10ef
Muchamad Aqiel Hilman Maulandany <maqiel2600@gmail.com>
2018-07-22 03:03:25 +0700

    add folder, drawable, and constant

--- app/src/main/java/maqielhm/appssamplegithub/main/MainActivity.java
+++ app/src/main/java/maqielhm/appssamplegithub/main/MainActivity.java
@@ -1,7 +1,9 @@
-package maqielhm.appssamplegithub;
+package maqielhm.appssamplegithub.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import maqielhm.appssamplegithub.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/maqielhm/appssamplegithub/utils/Constant.java
@@ -0,0 +1,5 @@
+package maqielhm.appssamplegithub.utils;
+
+public class Constant {
+    const static String API_KEY="250d605066114413d2f260fbc5af01ef";
+}
